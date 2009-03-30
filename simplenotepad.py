815383d874cefb23c28ce5625ff73a3ea363e822
Peter Keane <pkeane@mail.utexas.edu>
2009-03-29 22:03:38 -0500

    work

--- simplenotepad.py
+++ simplenotepad.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import datetime
+import markdown
 import os
 import random
+import re
 import string
 import sys
 import wsgiref.handlers
@@ -16,14 +18,22 @@ from google.appengine.ext.webapp.util import login_required
 # Set to true if we want to have our webapp print stack traces, etc
 _DEBUG = True
 
+def slugify(inStr):
+    removelist = ["a", "an", "as", "at", "before", "but", "by", "for","from","is", "in", "into", "like", "of", "off", "on", "onto","per","since", "than", "the", "this", "that", "to", "up", "via","with"];
+    for a in removelist:
+        aslug = re.sub(r'\b'+a+r'\b','',inStr)
+    aslug = re.sub('[^\w\s-]', '', aslug).strip().lower()
+    aslug = re.sub('\s+', '-', aslug)
+    return aslug
+
 class Note(db.Model):
   """Represents a single note.
   """
   note_title = db.TextProperty(required=True)
   note_text = db.TextProperty(required=True)
+  slug = db.StringProperty()
   created = db.DateTimeProperty(auto_now_add=True)
   updated = db.DateTimeProperty(auto_now=True)
-
 
 class BaseRequestHandler(webapp.RequestHandler):
   """Supplies a common template generation function.
@@ -46,38 +56,80 @@ class BaseRequestHandler(webapp.RequestHandler):
     path = os.path.join(directory, os.path.join('templates', template_name))
     self.response.out.write(template.render(path, values, debug=_DEBUG))
 
-
 class NotesPage(BaseRequestHandler):
   """Lists the notes """
 
   @login_required
   def get(self,id=0):
-    self.generate('index.html', {
-      'id': id,
-    })
+      notes = db.GqlQuery("SELECT * from Note ORDER BY created DESC");
+      self.generate('index.html', {
+          'notes': notes,
+      })
 
-  def post(self,id=0):
-    self.generate('index.html', {
-      'id': id,
-    })
+  def post(self):
+      note_title = self.request.get('note_title')
+      note_text =  self.request.get('note_text')
+      slug = slugify(note_title) 
+      if (note_text and note_title):
+          record = Note(note_title=note_title,note_text=note_text,slug=slug);
+          record.put()
+      self.redirect('/')
 
+class NoteEditPage(BaseRequestHandler):
+  @login_required
+  def get(self,key=0):
+      notes = db.GqlQuery("SELECT * from Note ORDER BY created DESC");
+      note = Note.get(key)
+      self.generate('edit.html', {
+          'note_title': note.note_title,
+          'note_text': note.note_text,
+          'note_key': key,
+          'notes' : notes,
+      })
+
+  def post(self,key=0):
+      note = Note.get(key)
+      note.note_title = self.request.get('note_title')
+      note.note_text =  self.request.get('note_text')
+      note.slug = slugify(note.note_title) 
+      note.put()
+      self.redirect('/note/'+note.slug)
+
+class NoteTextPage(BaseRequestHandler):
+  def get(self,slug=0):
+      q = Note.all()
+      q.filter('slug =',slug) 
+      md = markdown.Markdown()
+      res = q.fetch(1)
+      for note in res:
+          self.generate('note_text.html', {
+              'note_title': note.note_title,
+              'note_text': md.convert(note.note_text),
+          })
 
 class NotePage(BaseRequestHandler):
-  @login_required
-  def get(self,id=0):
-    self.generate('index.html', {
-      'id': id,
-    })
+  def get(self,slug=0):
+      notes = db.GqlQuery("SELECT * from Note ORDER BY created DESC");
+      q = Note.all()
+      q.filter('slug =',slug) 
+      md = markdown.Markdown()
+      res = q.fetch(1)
+      for note in res:
+          self.generate('note.html', {
+              'note_title': note.note_title,
+              'note_text': md.convert(note.note_text),
+              'notes': notes,
+          })
 
 def main():
   application = webapp.WSGIApplication([
     ('/', NotesPage),
-    ('/post', NotesPage),
-    ('/note', NotePage),
-    (r'/note/(.*)', NotePage),
+    ('/notes', NotesPage),
+    ('/edit/(.*)', NoteEditPage),
+    ('/text/(.*)', NoteTextPage),
+    ('/note/(.*)', NotePage),
   ], debug=_DEBUG)
   wsgiref.handlers.CGIHandler().run(application)
-
 
 if __name__ == '__main__':
   main()
